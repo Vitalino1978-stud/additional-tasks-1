bf99d981abbacba6519f020bc3af5d7a4553ed49
Vitalino78 <vitalinos1978@gmail.com>
2021-02-07 18:20:04 +0200

    starts

---
+++ tasks.js
@@ -0,0 +1,23 @@
+// // function formatMessage(message, maxLength) {
+// //   let result;
+// // Пиши код ниже этой строки
+// //   const str = message.split('')
+// //   if (str < maxLength) {
+// //     result = message;
+// //   return result
+  
+// //   } else {
+// //     message = str.join('') + '...';
+// //   // result = message;
+// // }
+
+//   const formatMessage = (message, maxLength) => {
+//   // let result;
+// // Пиши код ниже этой строки
+//  result = message.length < maxLength ? message : message.slice(0, maxLength)+'...';
+// // Пиши код выше этой строки
+//   // return result;
+// }
+// // Пиши код выше этой строки
+//   return result;
+
