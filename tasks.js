cf78299ccd7c6756f7b885395c7943f689cc9605
Vitalino78 <vitalinos1978@gmail.com>
2021-02-09 23:56:44 +0200

    total price keys

--- tasks.js
+++ tasks.js
@@ -267,3 +267,31 @@
 // for (const key in hotel) {
 //   console.log('Key: ', key);
 // }
+
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Сканер', price: 2700, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Пиши код ниже этой строки
+//   let totalPrice = 0;
+//    for (const value of products) {
+//      //console.log(value.quantity);
+//      if (value.name === productName) {
+//        totalPrice = (value.price) * (value.quantity);
+       
+//      }
+        
+//       //console.log(totalPrice); 
+     
+//     }
+  
+//   return totalPrice;
+//   // Пиши код выше этой строки
+// }
+
+// console.log(calculateTotalPrice('Радар')); 
+//  console.log(calculateTotalPrice('Дроид'));
