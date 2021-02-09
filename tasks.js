a477f3693341b4a6db90735248512df618b6d280
Vitalino78 <vitalinos1978@gmail.com>
2021-02-09 22:26:57 +0200

    price

--- tasks.js
+++ tasks.js
@@ -203,3 +203,25 @@
 // }
 // console.log(hexColors);
 // console.log(rgbColors);
+
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Сканер', price: 2700, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Пиши код ниже этой строки
+//   //const values = Object.values(products)
+//   for (const value of products) {
+// //console.log(value);
+//     if (value.name === productName) {
+//       return value.price;
+//     }
+//   }
+  
+  
+//   // Пиши код выше этой строки
+// }
+// console.log(getProductPrice('Радар'));
