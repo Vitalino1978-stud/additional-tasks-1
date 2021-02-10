b3dd0e747850c05eb1c7a597aa04b6c50db734a3
Vitalino78 <vitalinos1978@gmail.com>
2021-02-10 07:09:07 +0200

    Colours)

--- tasks.js
+++ tasks.js
@@ -307,3 +307,24 @@
 // //console.log(title);
 // //console.log(author);
 // console.log(rat);
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Пиши код ниже этой строки
+
+
+// for (const color of colors){
+//   const { hex, rgb } = color;
+//   console.log(hex);
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+// console.log(hexColors);
+// console.log(rgbColors);
