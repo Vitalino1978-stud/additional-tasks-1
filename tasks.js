915b6c6e9d6b31afff4191cf366fcbda8a1c5218
Vitalino78 <vitalinos1978@gmail.com>
2021-02-10 19:03:47 +0200

    Math min, max

--- tasks.js
+++ tasks.js
@@ -344,3 +344,11 @@
 // const {today:{low: lowToday, high: highToday, icon: todayIcon}, tomorrow: {low: lowTomorrow, high: highTomorrow, tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'},} = forecast;
 
 // console.log(tomorrowIcon);
+
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Пиши код ниже этой строки
+// const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
