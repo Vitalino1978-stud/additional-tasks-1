d669d2526f7d49e9eb497b66456ae099c0622c29
Vitalino78 <vitalinos1978@gmail.com>
2021-02-11 22:18:47 +0200

    !

--- tasks.js
+++ tasks.js
@@ -379,3 +379,16 @@
 //   return total;
 // }
 // console.log(add(12, 4, 11, 48));
+
+// function addOverNum(firstElement, ...args) {
+// let total = 0;
+// for (const arg of args) {
+// if (arg > firstElement) {
+// total += arg;
+// }
+// }
+// return total;
+// }
+// console.log(addOverNum(10, 12, 4, 11, 48, 10, 8)); //71
+// console.log(addOverNum(20, 74, 11, 62, 46, 12, 36));
+// console.log(addOverNum(50, 15, 27));
