ee0ddc4a7bff0cef4b6e12ef96f66431b49feda9
Vitalino78 <vitalinos1978@gmail.com>
2021-02-13 11:46:01 +0200

    hell task 7-4!!!

--- tasks.js
+++ tasks.js
@@ -532,3 +532,38 @@
 // pizzaPalace.order('Биг майк', makePizza, onOrderError);
 // pizzaPalace.order('Венская', makePizza, onOrderError);
 
+// Task7-4
+// Задание
+// Сервису приготовления и доставки еды требуется функция
+// генерации сообщений о статусе заказа.
+// Дополни функцию composeMessage(position) так,
+//   чтобы она возвращала строку в формате 'Готовим <блюдо> для <почта>. 
+// Ваш заказ < позиция > -й в очереди.' 
+// Позиция это значение параметра position - позиция элемента в массиве(на единицу больше чем индекс).
+
+// Не объявляй дополнительные параметры функции composeMessage(position).
+// Используй call для вызова функции в контексте одного объекта-заказа.
+// Используй this в теле функции для доступа к свойствам объекта - заказа в
+// контексте которого она была вызывана.
+// Дополни код так, чтобы в переменной messages, с помощью метода map,
+//   получился массив сообщений о статусе заказов из массива orders.
+
+// const orders = [
+//   { email: 'solomon@topmail.ua', dish: 'Burger' },
+//   { email: 'artemis@coldmail.net', dish: 'Pizza' },
+//   { email: 'jacob@mail.com', dish: 'Taco' },
+// ];
+
+// // Пиши код ниже этой строки
+// function composeMessage(position) {
+//   return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.` 
+// }
+
+// const messages = orders.map((order, index) => {
+//   return composeMessage.call(order, index+1);
+
+// })
+// // [order, index, array] 
+
+
+// console.table(messages);
