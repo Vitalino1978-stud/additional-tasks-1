3e8bdc62335b1169e6cb8fdf89e8a55064f23968
Vitalino78 <vitalinos1978@gmail.com>
2021-02-13 11:42:21 +0200

    task3-4

--- tasks.js
+++ tasks.js
@@ -466,3 +466,19 @@
 //   callback(string);
 // };
 // higherOrderFunction(printMessage);
+
+// Task3-4
+
+// function makePizza(pizzaName, callback) {
+//   console.log(`Пицца ${pizzaName} готовится, ожидайте...`);
+//   callback(pizzaName);
+// }
+
+// makePizza('Роял гранд', function deliverPizza(pizzaName) {
+//   console.log(`Доставляем пиццу ${pizzaName}.`);
+// });
+// // Пиши код ниже этой строки
+
+// makePizza('Ультрасыр', function eatPizza(pizzaName) {
+// console.log(`Едим пиццу ${pizzaName}.`);
+// });
