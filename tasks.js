7cf9dc3230923d7559bd525a666dbf1691e385d3
Vitalino78 <vitalinos1978@gmail.com>
2021-02-13 11:47:11 +0200

    task 9-4

--- tasks.js
+++ tasks.js
@@ -567,3 +567,37 @@
 
 
 // console.table(messages);
+
+// Task9-4
+// Задание
+// Функция composeMessage(customerName) создаёт приветственные сообщения для ресторанов.
+// Дополни код так, чтобы в переменных pizzaPalaceComposer и burgerShackComposer были её копии
+// с привязанным контекстом к соответствующим объектам.
+
+// function greet(clientName) {
+//   return `${clientName}, добро пожаловать в «${this.service}».`;
+// }
+// const steam = { service: 'Steam' };
+// const steamGreeter = greet.bind(steam);
+// steamGreeter('Манго'); // "Манго, добро пожаловать в «Steam»."
+// const gmail = { service: 'Gmail' };
+// const gmailGreeter = greet.bind(gmail);
+// gmailGreeter('Поли'); // "Поли, добро пожаловать в «Gmail»."
+
+// const pizzaPalace = {
+//   company: 'Pizza Palace',
+// };
+// const burgerShack = {
+//   company: 'Burger Shack',
+// };
+// function composeMessage(customerName) {
+//   return `${customerName}, всегда рады вас видеть в «${this.company}».`;
+// }
+// // Пиши код ниже этой строки
+
+// const pizzaPalaceComposer = composeMessage.bind(pizzaPalace);
+// const pizzaPalaceMessage = pizzaPalaceComposer('Манго');
+
+// const burgerShackComposer = composeMessage.bind(burgerShack);
+// const burgerShackMessage = burgerShackComposer('Поли');
+
