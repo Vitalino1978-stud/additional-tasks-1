bc40a6c26cb62bef1418bd67e1c8186424cbde81
Vitalino78 <vitalinos1978@gmail.com>
2021-02-13 11:44:15 +0200

    task 5-6

--- tasks.js
+++ tasks.js
@@ -482,3 +482,53 @@
 // makePizza('Ультрасыр', function eatPizza(pizzaName) {
 // console.log(`Едим пиццу ${pizzaName}.`);
 // });
+
+// Task4 - 4
+// Необходимо написать логику обработки заказа пиццы.
+// Выполни рефакторинг метода order так, чтобы он принимал вторым и третим параметрами два колбэка
+// onSuccess и onError.
+
+// Если в свойстве pizzas нет пиццы с названием из параметра pizzaName,
+//   метод order должен возвращать результат вызова колбэка onError, передавая ему аргументом строку
+// 'В ассортименте нет пиццы с названием <имя пиццы>.'
+// Если в свойстве pizzas есть пицца с названием из параметра pizzaName,
+//   метод order должен возвращать результат вызова колбэка onSuccess,
+//     передавая ему аргументом имя заказанной пиццы.
+// После объявления объекта pizzaPalace мы добавили колбэки и вызовы методов. Пожалуйста ничего там не меняй.
+
+// Тесты
+// Метод order объявляет три параметра.
+// Вызов pizzaPalace.order('Аль Копчино', makePizza, onOrderError) возвращает
+// 'Ваш заказ принят. Готовим пиццу Аль Копчино.'.
+// Вызов pizzaPalace.order('Четыре нарезона', makePizza, onOrderError) возвращает
+// 'Ваш заказ принят. Готовим пиццу Четыре нарезона.'.
+// Вызов pizzaPalace.order('Биг майк', makePizza, onOrderError) возвращает
+// 'Ошибка! В ассортименте нет пиццы с названием Биг майк.'.
+// Вызов pizzaPalace.order('Венская', makePizza, onOrderError) возвращает
+// 'Ошибка! В ассортименте нет пиццы с названием Венская.'.
+// const pizzaPalace = {
+//   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+//   order(pizzaName, onSuccess, onError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       return onSuccess(pizzaName);
+//     }
+//     else {
+//       return onError(pizzaName);
+//     };
+//   },
+// };
+// // Пиши код выше этой строки
+// // Колбэк для onSuccess
+// function makePizza(pizzaName) {
+//   return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+// }
+// // Колбэк для onError
+// function onOrderError(error) {
+//   return `Ошибка! В ассортименте нет пиццы с названием ${error}.`;
+// }
+// // Вызовы метода с колбэками
+// console.log(pizzaPalace.order('Аль Копчино', makePizza, onOrderError));
+// pizzaPalace.order('Четыре нарезона', makePizza, onOrderError);
+// pizzaPalace.order('Биг майк', makePizza, onOrderError);
+// pizzaPalace.order('Венская', makePizza, onOrderError);
+
