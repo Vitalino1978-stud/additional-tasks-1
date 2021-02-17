cd42ec22e930db68dfcbf5b32340775486555582
Vitalino78 <vitalinos1978@gmail.com>
2021-02-17 22:40:31 +0200

    5 module start

--- tasks.js
+++ tasks.js
@@ -635,3 +635,20 @@
 // console.log(secondInvoke);
 // // Почта poly@hotmail.de удалена из рассылки.
 // console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
+
+
+// module-5
+// const parent = {
+//   name: 'Stacey',
+//   surname: 'Moore',
+//   age: 54,
+//   heritage: 'Irish'
+// };
+// // Пиши код ниже этой строки
+
+// const child = Object.create(parent);
+  
+
+// // Пиши код выше этой строки
+// child.name = 'Jason';
+// child.age = 27;
