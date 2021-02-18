e4a39c7551edc79e3f735843861798d3564b9a3a
Vitalino78 <vitalinos1978@gmail.com>
2021-02-18 22:09:32 +0200

    t 10-5

--- tasks.js
+++ tasks.js
@@ -753,4 +753,16 @@
     
 // }
 // }
-
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getPrice() {
+//     return this.price;
+//   }
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+// }
