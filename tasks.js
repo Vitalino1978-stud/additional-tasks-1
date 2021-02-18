ddd15d7ed1a6cba03e3b9b18883f3e968d50a171
Vitalino78 <vitalinos1978@gmail.com>
2021-02-18 22:56:31 +0200

    t 14-5

--- tasks.js
+++ tasks.js
@@ -881,3 +881,40 @@
 // console.log(builder.getValue()); // '^.^'
 // builder.padBoth('=');
 // console.log(builder.getValue()); // '=^.^='
+
+// t 14-5
+// class Car {
+//   #model;
+//   #price;
+//   #brand;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   get model() {
+//     return this.#model;
+//   }
+
+//   set model(newModel) {
+//     this.#model = newModel;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+// }
