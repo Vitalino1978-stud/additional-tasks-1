f8c493fd5019892ffcd3e89937320dead3341f9f
Vitalino78 <vitalinos1978@gmail.com>
2021-02-21 23:24:09 +0200

    task 18-5

--- tasks.js
+++ tasks.js
@@ -1038,6 +1038,45 @@
 //   SUPERUSER: 'superuser' }
 // }
 
+// task 18-5
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   // Пиши код ниже этой строки
+//   accessLevel;
+
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser'
+//   };
+//   constructor({email, accessLevel}){
+//   super(email);
+//   }
+//   // Пиши код выше этой строки
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   accessLevel: Admin.AccessLevel.SUPERUSER
+// });
+
+// console.log(mango.email); // mango@mail.com
+// console.log(mango.accessLevel); // superuser
+
 
 // practice 20.022021
 
