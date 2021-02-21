297c64afeee011d1449b2dc2797805aa4860b612
Vitalino78 <vitalinos1978@gmail.com>
2021-02-21 22:51:17 +0200

    task 15-5

--- tasks.js
+++ tasks.js
@@ -956,7 +956,41 @@
 // console.log(audi.price); // 49000
 
 // audi.price = 51000;
+// console.log(audi.price); // 
+
+// task 15-5
+
+// class Car {
+//   // Пиши код ниже этой строки
+//   static MAX_PRICE = 50000;
+//   #price;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if (Car.MAX_PRICE <= newPrice){
+//     return this.#price;
+//    };
+//      this.#price = newPrice;
+//   }
+//   // Пиши код выше этой строки
+// }
+
+// const audi = new Car({price: 35000});
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
 // console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
 
 // practice 20.022021
 
