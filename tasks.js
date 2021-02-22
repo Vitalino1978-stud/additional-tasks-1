7f7410ef6a0c9378422f9e778f88cab66a90aa67
Vitalino78 <vitalinos1978@gmail.com>
2021-02-22 22:58:39 +0200

    task 6-2

--- tasks.js
+++ tasks.js
@@ -1284,4 +1284,19 @@
 
 //   // Пиши код выше этой строки
 //   return totalPrice;
-}
+// }
+
+// task 6-2
+// function filterArray(numbers, value) {
+//     const filteredNumbers = [];
+//     // Пиши код ниже этой строки
+  
+//     numbers.forEach(function(number) {
+//       if (number > value) {
+//         filteredNumbers.push(number);
+//       }
+//     })
+  
+//     // Пиши код выше этой строки
+//     return filteredNumbers;
+//   }
