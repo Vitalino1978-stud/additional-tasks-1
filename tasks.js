f4ad0f1dc7069e114da33601a9e39118cf8b6d99
Vitalino78 <vitalinos1978@gmail.com>
2021-03-04 22:17:54 +0200

    task 6-23 & 6-24

--- tasks.js
+++ tasks.js
@@ -1653,3 +1653,21 @@
 // const getActiveUsers = (users) => {
 //    return users.filter((user) => user.isActive === true); 
 // };
+// task 6-23
+// const getInactiveUsers = (users) => {
+ 
+//     return users.filter((user) => user.isActive !== true); 
+// }; 
+// task 6-24
+// const books = [
+//   { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//   { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//   { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//   { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+// ];
+// const BOOK_TITLE = 'Сон смешного человека';
+// const AUTHOR = 'Роберт Шекли';
+// // Пиши код ниже этой строки
+
+// const bookWithTitle = books.find((book) => book.title === BOOK_TITLE);
+// const bookByAuthor = books.find((book) => book.author === AUTHOR);
