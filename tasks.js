723307b3c1dda5e6fbc2bf3db3db5e62b7d8e4fc
Vitalino78 <vitalinos1978@gmail.com>
2021-03-25 14:15:19 +0200

    task 6-34

--- tasks.js
+++ tasks.js
@@ -1774,3 +1774,17 @@
 // }, 0);
 // };
 // Пиши код выше этой строки
+
+// task 6-34
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//   'Ли Танит',
+//   'Бернард Корнуэлл',
+//   'Роберт Шекли',
+//   'Федор Достоевский'
+// ];
+// // Пиши код ниже этой строки
+
+// const ascendingReleaseDates = [...releaseDates].sort();
+
+// const alphabeticalAuthors = [...authors].sort();
