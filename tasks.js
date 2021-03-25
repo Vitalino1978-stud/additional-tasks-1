8fae9094d192a43c1674a7e46ee83f979d332e78
Vitalino78 <vitalinos1978@gmail.com>
2021-03-25 14:09:03 +0200

    task 6-33

--- tasks.js
+++ tasks.js
@@ -1765,3 +1765,12 @@
 // }, 0);
 //   };
 // Пиши код выше этой строки
+
+// task 6-33
+// Пиши код ниже этой строки
+// const getTotalFriendCount = users => {
+//   return users.reduce((total, user) => {
+//     return total + user.friends.length;
+// }, 0);
+// };
+// Пиши код выше этой строки
