0889a0457996bcb211ad1eaf10db26a561a990c8
Vitalino78 <vitalinos1978@gmail.com>
2021-03-25 14:26:39 +0200

    task 6-35

--- tasks.js
+++ tasks.js
@@ -1788,3 +1788,11 @@
 // const ascendingReleaseDates = [...releaseDates].sort();
 
 // const alphabeticalAuthors = [...authors].sort();
+
+// task 6-35
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// // Пиши код ниже этой строки
+
+// const ascendingReleaseDates = [...releaseDates].sort((a, b) => a-b);
+
+// const descendingReleaseDates = [...releaseDates].sort((a, b) => b-a);
